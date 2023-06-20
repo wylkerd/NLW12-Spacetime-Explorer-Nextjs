e4a6c035f4119129bec43aeaed4af62662691477
wylkerd <wylkerd@hotmail.com>
2023-06-20 14:25:31 -0300

    Iniciado projeto mobile / Configurado e instalado Nativewind + Tailwind

---
+++ mobile/App.tsx
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import React from "react";
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="bg-gray-950 flex-1 items-center justify-center">
+      <Text className="text-gray-50 font-bold text-5xl">Hello world</Text>
+      <StatusBar style="light" translucent/>
+    </View>
+  );
+}
+
