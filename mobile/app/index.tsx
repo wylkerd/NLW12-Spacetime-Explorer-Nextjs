1f21017a8581e778581bff292b2bda9598e60c90
wylkerd <wylkerd@hotmail.com>
2023-06-22 21:55:09 -0300

    mobile: Navegando usuário com Expo Router ( Produz o mesmo funcionamento de rotas do Next,js )

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,5 +1,10 @@
 import { StatusBar } from 'expo-status-bar'
 import { ImageBackground, View, Text, TouchableOpacity } from 'react-native'
+import { useRouter } from 'expo-router'
+import { styled } from 'nativewind'
+import { useAuthRequest, makeRedirectUri } from 'expo-auth-session'
+import { useEffect } from 'react'
+
 import * as SecureStore from 'expo-secure-store'
 
 import {
@@ -10,13 +15,10 @@ import {
 
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 
-import blurBg from './src/assets/bg-blur.png'
-import Stripes from './src/assets/stripes.svg' // Impotacoes SVG virao componentes com a lib react-native-svg-transformer
-import NLWLogo from './src/assets/nlw-spacetime-logo.svg'
-import { styled } from 'nativewind'
-import { useAuthRequest, makeRedirectUri } from 'expo-auth-session'
-import { useEffect } from 'react'
-import { api } from './src/lib/api'
+import blurBg from '../src/assets/bg-blur.png'
+import Stripes from '../src/assets/stripes.svg' // Impotacoes SVG virao componentes com a lib react-native-svg-transformer
+import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
+import { api } from '../src/lib/api'
 
 // Hack de estilo para componentes nao nativos do React Native
 // Permite que eu use o Nativewind + Tailwind no compoente de SVG
@@ -31,13 +33,15 @@ const discovery = {
 }
 
 export default function App() {
+  const router = useRouter()
+
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   })
 
-  const [request, response, signInWithGitHub] = useAuthRequest(
+  const [, response, signInWithGitHub] = useAuthRequest(
     {
       clientId: '48f5484744669c001705',
       scopes: ['identity'],
@@ -47,6 +51,18 @@ export default function App() {
     },
     discovery,
   )
+
+  async function handleGithubOAuthCode(code: string) {
+    const response = await api.post('register', {
+      code,
+    })
+
+    const { token } = response.data
+
+    await SecureStore.setItemAsync('token', token)
+
+    router.push('/memories')
+  }
 
   useEffect(() => {
     // console.log(
@@ -59,18 +75,7 @@ export default function App() {
     if (response?.type === 'success') {
       const { code } = response.params
 
-      api
-        .post('register', {
-          code,
-        })
-        .then((response) => {
-          const { token } = response.data
-
-          SecureStore.setItemAsync('token', token)
-        })
-        .catch((error) => {
-          console.error(error)
-        })
+      handleGithubOAuthCode(code)
     }
   }, [response])
 
